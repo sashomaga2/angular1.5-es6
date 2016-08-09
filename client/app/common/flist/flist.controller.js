af285e0204e11bbf1fc6ac4cda1e7213257445a9
Alex <saswork@hotmail.com>
2016-08-09 20:26:56 +0300

    implemented basic view

--- client/app/common/flist/flist.controller.js
+++ client/app/common/flist/flist.controller.js
@@ -1,12 +1,19 @@
 class FListController {
-    constructor(flickDataService) {
-        // flickDataService.getData().then(result => {
-        //    this.items = result.data.items;
-        //    console.log('items', this.items);
-        // });
+    constructor($compile, $sce) {
+        let vm = this;
+
+        vm.test = (data) => {
+            console.log('data', data);
+        }
+
+        vm.convert = (html) => $sce.trustAsHtml(html);
+
+        vm.$postLink = function() {
+            console.log('elem', arguments);
+        }
     }
 }
 
-FListController.$inject = ['flickDataService'];
+FListController.$inject = ['$compile', '$sce'];
 
 export default FListController;
